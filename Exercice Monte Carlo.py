b17d59128202ef0d20200584d5757160ee1c77fe
TalyWeinb <weinbrenner.marion@hotmail.fr>
2018-06-14 17:11:04 +0200

    Exercice PO

--- Exercice Monte Carlo.py
+++ Exercice Monte Carlo.py
@@ -1,17 +1,30 @@
 import random
 import math
 
-iteration = 10
-total = 0
 
-for i in range(1,iteration):
-    x = random.random()
-    y = random.random()
-    dist = math.sqrt(x*x+y*y)
+#declare classe
+class PIMonteCarlo:
+    #déclare méthode
+    def computePI(self, iteration):
+        total = 0
 
-    if dist<=1:
-        total = total+1
+        for i in range(1,iteration):
+            x = random.random()
+            y = random.random()
+            dist = math.sqrt(x*x+y*y)
 
-pi = total / iteration *4
+        if dist<=1:
+            total = total+1
 
-print(pi)
+        pi = total / iteration *4
+
+        return pi
+
+#instancier la classe
+myClass = PIMonteCarlo()
+
+pi10 = myClass.computePI(10)
+pi1000 = myClass.computePI(1000)
+
+print(pi10)
+print(pi1000)
